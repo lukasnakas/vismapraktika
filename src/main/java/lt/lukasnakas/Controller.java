f490246b1c9f99cd733f2dc2f1918114f82f0d33
Lukas Nakas <lukas@LT-LIT-SC-0022.internal.visma.com>
2020-02-03 17:35:33 +0200

    added additional request mapping

--- src/main/java/lt/lukasnakas/Controller.java
+++ src/main/java/lt/lukasnakas/Controller.java
@@ -1,5 +1,6 @@
 package lt.lukasnakas;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,11 @@ public class Controller {
     @RequestMapping("/")
     public String Greet(){
         return "Hello!";
+    }
+
+    @RequestMapping("/banks/{id}")
+    public String getBank(@PathVariable String id){
+        return "Bankas" + id;
     }
 
 }
