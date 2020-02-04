00533a449381aa4d628f04ebb1b6b2458e83ff92
Lukas Nakas <lukas@LT-LIT-SC-0022.internal.visma.com>
2020-02-04 18:40:31 +0200

    access tokens should refresh when expired

--- src/main/java/lt/lukasnakas/Controller/Controller.java
+++ src/main/java/lt/lukasnakas/Controller/Controller.java
@@ -1,10 +1,12 @@
 package lt.lukasnakas.Controller;
 
 import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import lt.lukasnakas.Model.Bank;
 import lt.lukasnakas.Service.BankService;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -38,23 +40,61 @@ public class Controller {
 
     @RequestMapping("/banks/{id}/accounts")
     public String getAccounts(@PathVariable String id){
-
         Bank bank = bankService.getBankByID(Integer.parseInt(id));
+
         if(bank == null || bank.getAccessToken().equals(""))
             return "";
 
         String tokenType = "Bearer";
         String authorizationHeader = tokenType + " " + bank.getAccessToken();
-        String type = "";
+        String accounts = "";
+
+        accounts = attemptFetchingAccounts(bank, authorizationHeader);
+
+        if(accounts == null){
+            bank.setAccessToken(refreshAccessToken());
+            accounts = attemptFetchingAccounts(bank, authorizationHeader);
+        }
+
+        return accounts;
+    }
+
+    private String attemptFetchingAccounts(Bank bank, String authorizationHeader){
+        String response = "";
 
         try {
-            HttpResponse<String> httpResponse = Unirest.get(bank.getURL()).header("Authorization", authorizationHeader).asString();
-            type = httpResponse.getBody();
+            HttpResponse<JsonNode> httpResponse = Unirest.get(bank.getURL()).header("Authorization", authorizationHeader).asJson();
+
+            if(httpResponse == null || (httpResponse.getBody().getObject() != null && httpResponse.getBody().getObject().has("message")))
+                return null;
+
+            response = httpResponse.getBody().toString();
         } catch (UnirestException e) {
             e.printStackTrace();
         }
 
-        return type;
+        return response;
+    }
+
+    private String refreshAccessToken(){
+        String newAccessToken = "";
+
+        try {
+            HttpResponse<JsonNode> httpResponse = Unirest.post("https://sandbox-b2b.revolut.com/api/1.0/auth/token")
+                    .field("grant_type", "refresh_token")
+                    .field("client_id", "lLtj8AIjzoaPjgwW4OpDqJfVrMUoVDjGNY4XNMIETXI")
+                    .field("refresh_token", "oa_sand_uy7EROH9X5Gw6W_B3v9HSIS7SVtDFcOVYA2xeNN0QEQ")
+                    .field("client_assertion_type", "urn:ietf:params:oauth:client-assertion-type:jwt-bearer")
+                    .field("client_assertion", "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzYW5kYm94LWIyYi5yZXZvbHV0LmNvbSIsInN1YiI6ImxMdGo4QUlqem9hUGpnd1c0T3BEcUpmVnJNVW9WRGpHTlk0WE5NSUVUWEkiLCJhdWQiOiJodHRwczovL3Jldm9sdXQuY29tIiwiZXhwIjoxODkzNDU2MDAwfQ.G5MaRQJ6feATXLwrJIsRuKtguINOtkLJznUueN0iBXU74g3goTm6mnly_BgJsO_lDH4AuunDOCw_LFcWGvXTOkwKdgrkif3j83aZXWF6W0HjNTnNSX_fLq4268xH9GK6eJI0YE42eY1wikNDJEGePqqydkTdmX6t0_K5nEglcCY")
+                    .asJson();
+
+            newAccessToken = httpResponse.getBody().getObject().getString("access_token");
+        } catch (UnirestException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("New access token: " + newAccessToken);
+        return newAccessToken;
     }
 
 }
--- src/main/java/lt/lukasnakas/Service/BankService.java
+++ src/main/java/lt/lukasnakas/Service/BankService.java
@@ -11,7 +11,7 @@ public class BankService {
 
     public BankService(List<Bank> bankList) {
         this.bankList = bankList;
-        this.bankList.add(new Bank(1, "Revolut", "oa_sand_rJh8MDaseBsGEHl8QRZM3Kp7slW-qI_o8wD68I_5TYQ", "https://sandbox-b2b.revolut.com/api/1.0/accounts"));
+        this.bankList.add(new Bank(1, "Revolut", "oa_sand_jHcTy71JCD1zdUSI1A5AZaV1Ho5zkC753IW23MPUTFc", "https://sandbox-b2b.revolut.com/api/1.0/accounts"));
         this.bankList.add(new Bank(2, "Swedbank", "", ""));
     }
 
