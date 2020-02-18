c7a8344e2ebe8462e68ae8677c0458d4bcd60c61
Lukas Nakas <lukas@LT-LIT-SC-0022.internal.visma.com>
2020-02-18 09:32:37 +0200

    test fixes

--- src/test/java/lt/lukasnakas/DanskePaymentValidationServiceTest.java
+++ src/test/java/lt/lukasnakas/DanskePaymentValidationServiceTest.java
@@ -1,39 +1,32 @@
-//package lt.lukasnakas;
-//
-//import lt.lukasnakas.model.danske.transaction.DanskePayment;
-//import lt.lukasnakas.service.danske.DanskePaymentValidationService;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//@ExtendWith(MockitoExtension.class)
-//public class DanskePaymentValidationServiceTest {
-//
-//	@InjectMocks
-//	private DanskePaymentValidationService danskePaymentValidationService;
-//
-//	@Test
-//	public void isValid_shouldReturnTrue(){
-//		DanskePayment payment = new DanskePayment(100, "debit");
-//		boolean actual = danskePaymentValidationService.isValid(payment);
-//		assertTrue(actual);
-//	}
-//
-//	@Test
-//	public void isValidNoTemplate_shouldReturnFalse(){
-//		DanskePayment payment = new DanskePayment(100, null);
-//		boolean actual = danskePaymentValidationService.isValid(payment);
-//		assertFalse(actual);
-//	}
-//
-//	@Test
-//	public void isValidNoAmount_shouldReturnFalse(){
-//		DanskePayment payment = new DanskePayment(0, "credit");
-//		boolean actual = danskePaymentValidationService.isValid(payment);
-//		assertFalse(actual);
-//	}
-//
-//}
+package lt.lukasnakas;
+
+import lt.lukasnakas.model.Payment;
+import lt.lukasnakas.service.danske.DanskePaymentValidationService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class DanskePaymentValidationServiceTest {
+
+	@InjectMocks
+	private DanskePaymentValidationService danskePaymentValidationService;
+
+	@Test
+	public void isValid_shouldReturnTrue(){
+		Payment payment = new Payment(null, null, null, 110, null, null);
+		boolean actual = danskePaymentValidationService.isValid(payment);
+		assertTrue(actual);
+	}
+
+	@Test
+	public void isValidNoAmount_shouldReturnFalse(){
+		Payment payment = new Payment(null, null, null, 0, null, null);
+		boolean actual = danskePaymentValidationService.isValid(payment);
+		assertFalse(actual);
+	}
+
+}
--- src/test/java/lt/lukasnakas/RevolutPaymentValidationServiceTest.java
+++ src/test/java/lt/lukasnakas/RevolutPaymentValidationServiceTest.java
@@ -1,108 +1,91 @@
-//package lt.lukasnakas;
-//
-//import lt.lukasnakas.model.revolut.transaction.RevolutPayment;
-//import lt.lukasnakas.model.revolut.transaction.RevolutReceiver;
-//import lt.lukasnakas.service.revolut.RevolutPaymentValidationService;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//@ExtendWith(MockitoExtension.class)
-//public class RevolutPaymentValidationServiceTest {
-//
-//	@InjectMocks
-//	private RevolutPaymentValidationService revolutPaymentValidationService;
-//
-//	@Test
-//	public void isValid_shouldReturnTrue(){
-//		RevolutPayment payment = new RevolutPayment(100, "123",
-//				new RevolutReceiver("789", "123456"),
-//				"ref", "456", "EUR");
-//		boolean actual = revolutPaymentValidationService.isValid(payment);
-//		assertTrue(actual);
-//	}
-//
-//	@Test
-//	public void isValidNoAmount_shouldReturnFalse(){
-//		RevolutPayment payment = new RevolutPayment(0, "123",
-//				new RevolutReceiver("789", "123456"),
-//				"ref", "456", "EUR");
-//		boolean actual = revolutPaymentValidationService.isValid(payment);
-//		assertFalse(actual);
-//	}
-//
-//	@Test
-//	public void isValidNegativeAmount_shouldReturnFalse(){
-//		RevolutPayment payment = new RevolutPayment(-10, "123",
-//				new RevolutReceiver("789", "123456"),
-//				"456", "ref", "EUR");
-//		boolean actual = revolutPaymentValidationService.isValid(payment);
-//		assertFalse(actual);
-//	}
-//
-//	@Test
-//	public void isValidNoCurrency_shouldReturnFalse(){
-//		RevolutPayment payment = new RevolutPayment(0, "123",
-//				new RevolutReceiver("789", "123456"),
-//				"456", "ref", "EUR");
-//		boolean actual = revolutPaymentValidationService.isValid(payment);
-//		assertFalse(actual);
-//	}
-//
-//	@Test
-//	public void isValidNoAccountId_shouldReturnFalse(){
-//		RevolutPayment payment = new RevolutPayment(0, "123",
-//				new RevolutReceiver("789", "123456"),
-//				null, "ref", "EUR");
-//		boolean actual = revolutPaymentValidationService.isValid(payment);
-//		assertFalse(actual);
-//	}
-//
-//	@Test
-//	public void isValidNoRevolutReceiver_shouldReturnFalse(){
-//		RevolutPayment payment = new RevolutPayment(0, "123",
-//				null, "456", "123", "EUR");
-//		boolean actual = revolutPaymentValidationService.isValid(payment);
-//		assertFalse(actual);
-//	}
-//
-//	@Test
-//	public void isValidNoRevolutReceiverCounterpartyId_shouldReturnFalse(){
-//		RevolutPayment payment = new RevolutPayment(0, "123",
-//				new RevolutReceiver(null, "123456"),
-//				"ref", "456", "EUR");
-//		boolean actual = revolutPaymentValidationService.isValid(payment);
-//		assertFalse(actual);
-//	}
-//
-//	@Test
-//	public void isValidNoRevolutReceiverAccountId_shouldReturnFalse(){
-//		RevolutPayment payment = new RevolutPayment(0, "123",
-//				new RevolutReceiver("789", null),
-//				"ref", "456", "EUR");
-//		boolean actual = revolutPaymentValidationService.isValid(payment);
-//		assertFalse(actual);
-//	}
-//
-//	@Test
-//	public void isValidNoReference_shouldReturnFalse(){
-//		RevolutPayment payment = new RevolutPayment(0, "123",
-//				new RevolutReceiver("789", "123456"),
-//				"ref", null, "EUR");
-//		boolean actual = revolutPaymentValidationService.isValid(payment);
-//		assertFalse(actual);
-//	}
-//
-//	@Test
-//	public void isValidNoType_shouldReturnFalse(){
-//		RevolutPayment payment = new RevolutPayment(0, "123",
-//				new RevolutReceiver("789", "123456"),
-//				"ref", "456", null);
-//		boolean actual = revolutPaymentValidationService.isValid(payment);
-//		assertFalse(actual);
-//	}
-//
-//}
+package lt.lukasnakas;
+
+import lt.lukasnakas.model.revolut.transaction.RevolutPayment;
+import lt.lukasnakas.model.revolut.transaction.RevolutReceiver;
+import lt.lukasnakas.service.revolut.RevolutPaymentValidationService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class RevolutPaymentValidationServiceTest {
+
+	@InjectMocks
+	private RevolutPaymentValidationService revolutPaymentValidationService;
+
+	@Test
+	public void isValid_shouldReturnTrue(){
+		RevolutPayment payment = new RevolutPayment("123", new RevolutReceiver("789", "123456"),
+				"EUR", "ref", 100);
+		boolean actual = revolutPaymentValidationService.isValid(payment);
+		assertTrue(actual);
+	}
+
+	@Test
+	public void isValidNoAmount_shouldReturnFalse(){
+		RevolutPayment payment = new RevolutPayment("123", new RevolutReceiver("789", "123456"),
+				"EUR", "ref", 0);
+		boolean actual = revolutPaymentValidationService.isValid(payment);
+		assertFalse(actual);
+	}
+
+	@Test
+	public void isValidNegativeAmount_shouldReturnFalse(){
+		RevolutPayment payment = new RevolutPayment("123", new RevolutReceiver("789", "123456"),
+				"EUR", "ref", -100);
+		boolean actual = revolutPaymentValidationService.isValid(payment);
+		assertFalse(actual);
+	}
+
+	@Test
+	public void isValidNoCurrency_shouldReturnFalse(){
+		RevolutPayment payment = new RevolutPayment("123", new RevolutReceiver("789", "123456"),
+				null, "ref", 100);
+		boolean actual = revolutPaymentValidationService.isValid(payment);
+		assertFalse(actual);
+	}
+
+	@Test
+	public void isValidNoAccountId_shouldReturnFalse(){
+		RevolutPayment payment = new RevolutPayment(null, new RevolutReceiver("789", "123456"),
+				"EUR", "ref", 100);
+		boolean actual = revolutPaymentValidationService.isValid(payment);
+		assertFalse(actual);
+	}
+
+	@Test
+	public void isValidNoRevolutReceiver_shouldReturnFalse(){
+		RevolutPayment payment = new RevolutPayment("123", null,
+				"EUR", "ref", 100);
+		boolean actual = revolutPaymentValidationService.isValid(payment);
+		assertFalse(actual);
+	}
+
+	@Test
+	public void isValidNoRevolutReceiverCounterpartyId_shouldReturnFalse(){
+		RevolutPayment payment = new RevolutPayment("123", new RevolutReceiver(null,
+				"123456"), "EUR", "ref", 100);
+		boolean actual = revolutPaymentValidationService.isValid(payment);
+		assertFalse(actual);
+	}
+
+	@Test
+	public void isValidNoRevolutReceiverAccountId_shouldReturnFalse(){
+		RevolutPayment payment = new RevolutPayment("123", new RevolutReceiver("789",
+				null), "EUR", "ref", 100);
+		boolean actual = revolutPaymentValidationService.isValid(payment);
+		assertFalse(actual);
+	}
+
+	@Test
+	public void isValidNoReference_shouldReturnFalse(){
+		RevolutPayment payment = new RevolutPayment("123", new RevolutReceiver("789", "123456"),
+				"EUR", null, 100);
+		boolean actual = revolutPaymentValidationService.isValid(payment);
+		assertFalse(actual);
+	}
+
+}
